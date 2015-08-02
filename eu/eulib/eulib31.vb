32c1935eefce44a174da81f1ecf40d8f76a0bde8
captainkout@gmail.com <captainkout@gmail.com>
2015-08-02 20:05:41 -0300

    updating getcombs

--- eu/eulib/eulib31.vb
+++ eu/eulib/eulib31.vb
@@ -1,20 +1,17 @@
 ﻿Public Class eulib31
-	Dim pcoin As New List(Of Integer) From {1, 2, 5} 'possible coins
-	Dim start As New List(Of Integer) 'list of all ones
+	Dim pcoin As New List(Of Integer) From {5, 2, 1} 'possible coins
+	Dim start As New Dictionary(Of Integer, Integer)
 	Dim combinations As New Dictionary(Of Integer, List(Of Dictionary(Of Integer, Integer))) 'all possible dcoins
 
 	Public Sub Main()
-		'init_dcoin(dcoin)
-
-		'initiate start arr
-		For i = 1 To 5
-			start.Add(1)
-		Next
+		start.Add(5, 1)
+		start.Add(2, 0)
+		start.Add(1, 0)
 
 		Dim dcoin As New Dictionary(Of Integer, Integer)
 		init_dcoin(dcoin)
 
-		getCombs(start, dcoin, False)
+		getCombs(start, 5)
 
 	End Sub
 	Public Sub init_dcoin(ByRef dic As Dictionary(Of Integer, Integer))
@@ -24,10 +21,19 @@
 		Next
 
 	End Sub
-	Public Sub getCombs()
+	Public Sub getCombs(ByRef dic As Dictionary(Of Integer, Integer), _
+											ByVal total As Integer)
+		Dim newdic As New Dictionary(Of Integer, Integer)
 
+		If combinations.Item(total).Contains(dic) Then
+			'add the coin to all the combinations and add to combinations
+			For Each item In combinations.Item(total)
+				'newdic =
+				'newdic() = newdic() + 1
+				combinations.Item(total).Add(newdic)
+			Next
+		End If
 
-		getCombs()
 
 	End Sub
 End Class
