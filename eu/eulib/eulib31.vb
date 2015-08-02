1aa9b0f5690c264706d6c33ef3486154f1afbe48
captainkout@gmail.com <captainkout@gmail.com>
2015-08-02 18:27:58 -0300

    need a new idea for my loop

--- eu/eulib/eulib31.vb
+++ eu/eulib/eulib31.vb
@@ -1,8 +1,7 @@
 ﻿Public Class eulib31
 	Dim pcoin As New List(Of Integer) From {1, 2, 5} 'possible coins
-	'Dim dcoin As New Dictionary(Of Integer, Integer)
 	Dim start As New List(Of Integer) 'list of all ones
-	Dim combinations As New List(Of Dictionary(Of Integer, Integer)) 'all possible dcoins
+	Dim combinations As New Dictionary(Of Integer, List(Of Dictionary(Of Integer, Integer))) 'all possible dcoins
 
 	Public Sub Main()
 		'init_dcoin(dcoin)
@@ -15,7 +14,7 @@
 		Dim dcoin As New Dictionary(Of Integer, Integer)
 		init_dcoin(dcoin)
 
-		getCombs(start, dcoin)
+		getCombs(start, dcoin, False)
 
 	End Sub
 	Public Sub init_dcoin(ByRef dic As Dictionary(Of Integer, Integer))
@@ -25,23 +24,10 @@
 		Next
 
 	End Sub
-	Public Sub getCombs(ByRef lst As List(Of Integer), _
-											ByRef dcoin2 As Dictionary(Of Integer, Integer))
+	Public Sub getCombs()
 
 
-		Dim newlst As New List(Of Integer)
-
-		For i = 0 To lst.Count - 1
-			Dim coin As Integer = lst(i)
-
-			If coin > lst.Count - 1 Then
-				'find combinations for everything after taking out "key" 1's and regress the list that left over
-				dcoin2(coin) = dcoin2(coin) + 1
-				newlst = lst.GetRange(lst.Count - 1 - coin, coin)
-				getCombs(newlst, dcoin2)
-			End If
-		Next
-
+		getCombs()
 
 	End Sub
 End Class
