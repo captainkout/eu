17dc93b5e0b38cfd2d42cae065c4f0ac3b6fb067
captainkout <captainkout@gmail.com>
2015-08-04 13:02:18 -0400

    really finished 31 this time

--- eu/eu/Module1.vb
+++ eu/eu/Module1.vb
@@ -2,14 +2,10 @@
 Module Module1
 
 	Sub Main()
-		'Dim x As New eulib31b
-		'x.Main()
-		'Console.WriteLine("friggin done fast!!")
-		''Console.ReadKey()
 
 		Dim y As New eulib31
 		y.Main()
-		Console.WriteLine("friggin done slow!!")
+		Console.WriteLine("friggin done!!")
 		Console.ReadKey()
 
 	End Sub
--- eu/eulib/eulib31.vb
+++ eu/eulib/eulib31.vb
@@ -1,80 +1,24 @@
-﻿Public Class eulib31
+﻿
+Public Class eulib31
 	Dim pcoin As New List(Of Integer) From {1, 2, 5, 10, 20, 50, 100, 200} 'possible coins
-	Dim start As New Dictionary(Of Integer, Integer)
-	Dim combinations As New Dictionary(Of Integer, List(Of Dictionary(Of Integer, Integer))) 'all possible dcoins
+	Dim combinations As New List(Of Integer)	'combinations for given i
 
 	Public Sub Main()
-		'add the zero case to combinations
-		Dim tmpList_zero As New List(Of Dictionary(Of Integer, Integer))
-		Dim tmpDic_zero As New Dictionary(Of Integer, Integer)
-		init_dic(tmpDic_zero)
-		tmpList_zero.Add(tmpDic_zero)
 
-		combinations.Add(0, tmpList_zero)		'this is the base case
-
-
-		For i = 1 To 9
-
-			Dim tmpList As New List(Of Dictionary(Of Integer, Integer))
-			Dim j = 0
-			While i >= pcoin(j)
-
-				If pcoin(j) = 1 Then
-					'get all combinations and add a 1 to it
-					For Each comb In combinations.Item(i - 1)
-						'add 1's
-						Dim tmpdic As New Dictionary(Of Integer, Integer)
-						For Each coin2 In pcoin
-							tmpdic(coin2) = comb(coin2)
-						Next
-						tmpdic(1) = tmpdic(1) + 1
-
-						tmpList.Add(tmpdic)
-					Next
-
-				ElseIf pcoin(j) = 2 Then
-					'get all combinations for total-coin with zero 1's
-					For Each comb In combinations.Item(i - pcoin(j))
-						If comb(1) = 0 Then
-							'add coin to it
-							Dim tmpdic As New Dictionary(Of Integer, Integer)
-							For Each coin2 In pcoin
-								tmpdic(coin2) = comb(coin2)
-							Next
-							tmpdic(pcoin(j)) = tmpdic(pcoin(j)) + 1
-
-							tmpList.Add(tmpdic)
-						End If
-					Next
-				ElseIf pcoin(j) = 5 Then
-					For Each comb In combinations.Item(i - pcoin(j))
-						If comb(1) = 0 AndAlso comb(2) = 0 Then
-							'add coin to it
-							Dim tmpdic As New Dictionary(Of Integer, Integer)
-							For Each coin2 In pcoin
-								tmpdic(coin2) = comb(coin2)
-							Next
-							tmpdic(pcoin(j)) = tmpdic(pcoin(j)) + 1
-
-							tmpList.Add(tmpdic)
-						End If
-					Next
-				End If
-				j = j + 1
-			End While
-			combinations.Add(i, tmpList)
-			Console.WriteLine("item:{0}   combinations:{1}", i, combinations.Item(i).Count())
+		For x = 0 To 200
+			If x = 0 Then
+				combinations.Add(1)
+			Else
+				combinations.Add(0)
+			End If
 		Next
-
-		For Each c In combinations.Item(7)
-			Console.WriteLine("1:{0}   2:{1}   5:{2}", c.Item(1), c.Item(2), c.Item(5))
+		For Each coin In pcoin
+			For i = coin To 200
+				combinations(i) = combinations(i) + combinations(i - coin)
+			Next
 		Next
-
-	End Sub
-	Public Sub init_dic(ByRef dic As Dictionary(Of Integer, Integer))
-
-		For i = 0 To pcoin.Count() - 1
-			dic.Add(pcoin(i), 0)
+		For i = 0 To combinations.Count() - 1
+			Console.WriteLine("i:{0}   comb:{1}", i, combinations(i))
 		Next
 
 	End Sub
