6162ed4d8f9e07b8f7ff215a679179ef64b56cb1
captainkout@gmail.com <captainkout@gmail.com>
2015-08-05 20:15:32 -0300

    finished 34, 0! =1 you jerkoff

--- eu/eulib/eulib34.vb
+++ eu/eulib/eulib34.vb
@@ -1,16 +1,9 @@
 ﻿Public Class eulib34
 	Public Sub Main()
-		Dim ninebang As Integer = fac(9)
 		Dim n As Integer = 1
-		Dim num As String = "9"
-		Dim curious As List(Of Integer)
-		While n * ninebang >= Val(num)
-			n = n + 1
-			num = num + "9"
-		End While
-		Console.WriteLine("n:{0}   n*ninebang:{1}   num:{2}", n, n * ninebang, num)
+		Dim curious As New List(Of Integer)
 
-		For i = 3 To 7 * ninebang
+		For i = 3 To 1999999 'this is last number less than facsum
 			If Valid(i) Then
 				curious.Add(i)
 				Console.WriteLine(i)
@@ -21,8 +14,8 @@
 	End Sub
 	Public Function Valid(ByVal num As Integer)
 		Dim value As Integer = 0
-		For Each dig In num.ToString()
-			value = value + fac(Val(dig))
+		For i = 0 To num.ToString().Length() - 1
+			value = value + fac(Val(num.ToString()(i)))
 		Next
 		If value = num Then
 			Return True
@@ -31,9 +24,13 @@
 		End If
 	End Function
 	Public Function fac(ByVal n As Integer)
-		For i = 1 To n
-			n = n * i
+		If n = 0 Then Return 1
+		If n = 1 Then Return 1
+		Dim dfac As Integer = 1
+		For i = 2 To n
+			dfac = dfac * i
 		Next
-		Return fac
+
+		Return dfac
 	End Function
 End Class
