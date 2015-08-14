c44cd1dc7bf1f05d8a0300c25b16507713bfac06
captainkout <captainkout@gmail.com>
2015-08-14 15:49:34 -0400

    poker stuff

--- eu/eulib50-59/eulib54.vb
+++ eu/eulib50-59/eulib54.vb
@@ -1,125 +1,181 @@
 ﻿Imports System.IO
 Public Class eulib54
-	Public Property dic As New List(Of String) From {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"}
+	Public Property CardDic As New List(Of String) From {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"}
+	Public Property SuitDic As New List(Of String) From {"C", "D", "H", "S"}
+
+	Public Enum Suit
+		hearts
+		diamonds
+		clubs
+		spades
+	End Enum
 	Public Sub Main()
-		Dim f As New StreamReader("c:\\users\\chris\\downloads\\p054_poker.txt")
-		'Dim number As New List(Of String) From {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"}
-		While True
+		'Dim f As New StreamReader("c:\\users\\chris\\downloads\\p054_poker.txt")
+		Dim f As New StreamReader("c:\\users\\ckoutras\\downloads\\p054_poker.txt")
+		Dim hand As New List(Of String)(f.ReadLine().Split(" "))
 
+		While hand.Count() > 0
 
-			Dim hand As New List(Of String)(f.ReadLine().Split(" "))
-			Dim A As New List(Of String), B As New List(Of String)
-			Dim acount As Integer = 0
-			A = hand.GetRange(0, 5)
-			B = hand.GetRange(5, 5)
+			Dim A As New Dictionary(Of String, Object), B As New Dictionary(Of String, Object)
+			Dim awins As Integer = 0
 
-			Dim aisflush = IsFlush(A)
-			Dim bisflush = IsFlush(B)
+			A("cards") = hand.GetRange(0, 5)
+			B("cards") = hand.GetRange(5, 5)
 
-			If aisflush.Item1() = True AndAlso bisflush.Item1() = True Then
-				For i = aisflush.Item2().Count() - 1 To 0
-					If aisflush.Item2(i) > bisflush.Item2(i) Then
-						acount = acount + 1
-					ElseIf aisflush.Item2(i) < bisflush.Item2(i) Then
-						Exit For
+			A("card count") = CardCount(A("cards"))
+			B("card count") = CardCount(B("cards"))
+
+			A("suit count") = SuitCount(A("cards"))
+			B("suit count") = SuitCount(B("cards"))
+
+			A("is flush") = A("suit count").Contains(5)
+			B("is flush") = B("suit count").Contains(5)
+
+			A("is straight") = DirectCast(A("card count"), List(Of Integer)).Max() = 1 AndAlso A("card count").LastIndexOf(1) - A("card count").IndexOf(1) = 5
+			B("is straight") = DirectCast(A("card count"), List(Of Integer)).Max() = 1 AndAlso B("card count").LastIndexOf(1) - B("card count").IndexOf(1) = 5
+
+			A("sets") = New List(Of Integer) From {A("card count").IndexOf(4), _
+																								 A("card count").IndexOf(3), _
+																								 A("card count").LastIndexOf(2), _
+																								 A("card count").LastIndexOf(2) - A("card count").IndexOf(2)}
+			B("sets") = New List(Of Integer) From {B("card count").IndexOf(4), _
+																								 B("card count").IndexOf(3), _
+																								 B("card count").LastIndexOf(2), _
+																								 B("card count").LastIndexOf(2) - B("card count").IndexOf(2)}
+
+			Select Case True
+				'check for straight flush
+				Case (A("is flush") = True AndAlso A("is straight") = True) OrElse _
+					(B("is flush") = True AndAlso B("is straight") = True)
+
+					If (A("is flush") = True AndAlso A("is straight") = True) AndAlso _
+					(B("is flush") = True AndAlso B("is straight") = True) Then
+						awins = awins + HighCard(A("card count"), B("card count"))
+					ElseIf (A("is flush") = True AndAlso A("is straight") = True) Then
+						awins = awins + 1
 					End If
-				Next
-			ElseIf aisflush.Item1() = True AndAlso bisflush.Item1() = False Then
-				acount = acount + 1
-			ElseIf aisflush.Item1() = False AndAlso bisflush.Item1() = True Then
-				Continue While
-				'go to next hand
-			End If
+					'bwins so don't add, just exit
+					Exit Select	'this needs to be exit for instead of sub
 
+					'check for quads
+				Case A("sets")(0) OrElse B("sets")(0)
 
+					If A("sets")(0) > 0 AndAlso B("sets")(0) > 0 Then
+						If A("sets")(0) = B("sets")(0) Then
+							awins = awins + HighCard(A("card count"), B("card count"))
+						ElseIf A("sets")(0) > B("sets")(0) Then
+							awins = awins + 1
+						End If
+					End If
+					Exit Select
+
+					'check for full house
+				Case (A("sets")(1) > 0 AndAlso A("sets")(2) > 0) OrElse _
+					(B("sets")(1) > 0 AndAlso B("sets")(2) > 0)
+
+					If (A("sets")(1) > 0 AndAlso A("sets")(2) > 0) AndAlso _
+					(B("sets")(1) > 0 AndAlso B("sets")(2) > 0) Then
+						awins = awins + HighCard(A("card count"), B("card count"))
+					ElseIf A("sets")(1) > 0 AndAlso A("sets")(2) > 0 Then
+						awins = awins + 1
+					End If
+					'bwins
+					Exit Select
+
+					'check flush
+				Case A("is flush") = True OrElse B("is flush") = True
+
+					If A("is flush") = True AndAlso B("is flush") = True Then
+						awins = awins + HighCard(A("card count"), B("card count"))
+					ElseIf A("is flush") = True Then
+						awins = awins + 1
+					End If
+					'bwins
+					Exit Select
+
+				Case A("is straight") = True OrElse B("is straight")
+
+					If A("is straight") = True AndAlso A("is straight") = True Then
+						awins = awins + HighCard(A("card count"), B("card count"))
+					ElseIf A("is straight") = True Then
+						awins = awins + 1
+					End If
+					'bwins
+					Exit Select
+
+					'check trips
+				Case A("sets")(1) > 0 OrElse B("sets")(1) > 0
+
+					If A("sets")(1) > B("sets")(1) Then
+						awins = awins + HighCard(A("card count"), B("card count"))
+					ElseIf A("sets")(1) > 0 Then
+						awins = awins + 1
+					End If
+					'bwins
+					Exit Select
+
+					'check twopair
+				Case (A("sets")(2) > 0 AndAlso A("sets")(3) > 0) OrElse _
+					(B("sets")(2) > 0 AndAlso B("sets")(3) > 0)
+
+					If A("sets")(2) = B("sets")(2) AndAlso _
+					A("sets")(3) = B("sets")(3) Then
+						awins = awins + HighCard(A("card count"), B("card count"))
+					ElseIf A("sets")(2) > B("sets")(2) OrElse _
+						(A("sets")(2) = B("sets")(2) AndAlso A("sets")(3) > B("sets")(3)) Then
+						awins = awins + 1
+					End If
+					'bwins
+					Exit Select
+
+					'check pair
+				Case A("sets")(2) > 0 OrElse B("sets")(2) > 0
+
+					If A("sets")(2) = B("sets")(2) Then
+						awins = awins + HighCard(A("card count"), B("card count"))
+					ElseIf A("sets")(2) > B("sets")(2) Then
+						awins = awins + 1
+					End If
+					'bwins
+					Exit Select
+
+				Case True
+					awins = awins + HighCard(A("card count"), B("card count"))
+
+			End Select
+
+			hand = New List(Of String)(f.ReadLine().Split(" "))
 		End While
-
-
 	End Sub
 
-	Public Function IsStraight(ByVal clst As List(Of String)) As Tuple(Of Integer, Integer)
-		Dim diglst As New List(Of Integer)
+	Public Function CardCount(ByVal clst As List(Of String))
+		Dim cards = New List(Of Integer) From {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
 		For Each c In clst
-			diglst.Add(dic.IndexOf(c.Substring(0, 1)))
+			cards(CardDic.IndexOf(c.Substring(0, 1))) = cards(CardDic.IndexOf(c.Substring(0, 1))) + 1
 		Next
-		diglst.Sort()
+		Console.WriteLine("{0},{1}", cards.GetType(), cards.Max())
 
-		For i = 0 To diglst.Count() - 2
-			If diglst(i) <> diglst(i + 1) Then
-				Return New Tuple(Of Integer, Integer)(1, diglst.Max())
-			End If
-		Next
-
-		Return New Tuple(Of Integer, Integer)(0, 0)
-	End Function
-	Public Function IsFlush(ByVal clst As List(Of String)) As Tuple(Of Integer, List(Of Integer))
-		Dim diglst As New List(Of Integer)
-
-		Dim poss As New List(Of String) From {"H", "D", "C", "S"}
-
-		For i = 0 To poss.Count() - 1
-			Dim count As Integer = 0
-			For Each c In clst
-				If c.Contains(poss(i)) = True Then
-					count = count + 1
-				End If
-			Next
-			If count = 5 Then
-				'is flush, get high card
-				For Each c In clst
-					diglst.Add(dic.IndexOf(c.Substring(1, 1)))
-				Next
-				diglst.Sort()
-				Return New Tuple(Of Boolean, List(Of Integer))(True, diglst)
-			End If
-		Next
-		Return New Tuple(Of Boolean, List(Of Integer))(False, Nothing)
-	End Function
-	Public Function IsQuad(ByVal clst As List(Of Integer)) As Tuple(Of Boolean, Integer)
-		If clst.Contains(4) Then
-			Return New Tuple(Of Boolean, Integer)(True, clst.IndexOf(4))
-		End If
-		Return New Tuple(Of Boolean, Integer)(False, 0)
-	End Function
-	Public Function IsFull(ByVal clst As List(Of Integer)) As Tuple(Of Boolean, Integer)
-		If clst.Contains(3) AndAlso clst.Contains(2) Then
-			Return New Tuple(Of Boolean, Integer)(True, clst.IndexOf(3))
-		End If
-		Return New Tuple(Of Boolean, Integer)(False, 0)
-	End Function
-	Public Function IsTrip(ByVal clst As List(Of Integer)) As Tuple(Of Boolean, Integer, Integer, Integer)
-		If clst.Contains(3) AndAlso clst.Contains(1) Then
-			Return New Tuple(Of Boolean, Integer, Integer, Integer)(True, clst.IndexOf(3), clst.LastIndexOf(1), clst.IndexOf(1))
-		End If
-		Return New Tuple(Of Boolean, Integer, Integer, Integer)(False, 0, 0, 0)
-	End Function
-	Public Function IsTwoPairIsTrip(ByVal clst As List(Of Integer)) As Tuple(Of Boolean, Integer, Integer, Integer)
-		If clst.IndexOf(2) <> clst.LastIndexOf(2) Then
-			Return New Tuple(Of Boolean, Integer, Integer, Integer)(True, clst.LastIndexOf(2), clst.IndexOf(2), clst.IndexOf(1))
-		End If
-		Return New Tuple(Of Boolean, Integer, Integer, Integer)(False, 0, 0, 0)
-	End Function
-	Public Function IsPair(ByVal clst As List(Of Integer)) As Tuple(Of Boolean, Integer, Integer, Integer, Integer)
-		Dim hand As New List(Of Integer)
-		If clst.IndexOf(2) = clst.LastIndexOf(2) Then
-			For i = 12 To 0 Step -1
-				If clst(i) = 1 Then
-					For j = 1 To clst(i)
-						hand.Add(i)
-					Next
-				End If
-			Next
-			Return New Tuple(Of Boolean, Integer, Integer, Integer, Integer)(True, clst.IndexOf(2), hand(0), hand(1), hand(2))
-		End If
-		Return New Tuple(Of Boolean, Integer, Integer, Integer, Integer)(True, clst.IndexOf(2), hand(0), hand(1), hand(2))
-	End Function
-	Public Function Ccount(ByVal clst As List(Of String)) As List(Of Integer)
-		Dim cards As New List(Of Integer) From {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-
-		For Each c In clst
-			cards(dic.IndexOf(c.Substring(0, 1))) = cards(dic.IndexOf(c.Substring(0, 1))) + 1
-		Next
 		Return cards
+	End Function
+	Public Function SuitCount(ByVal clst As List(Of String)) As List(Of Integer)
+		Dim suits As New List(Of Integer) From {0, 0, 0, 0}
+
+		For Each c In clst
+			suits(SuitDic.IndexOf(c.Substring(1, 1))) = suits(SuitDic.IndexOf(c.Substring(1, 1))) + 1
+		Next
+		Return suits.GetRange(0, suits.Count())
+	End Function
+	Public Function HighCard(ByVal ACardCount As List(Of Integer), _
+													 ByVal BCardCount As List(Of Integer)) As Integer
+		For i = 13 To 0 Step -1
+			If ACardCount(i) > BCardCount(i) Then
+				Return 1
+			ElseIf ACardCount(i) < BCardCount(i) Then
+				Return 0
+			End If
+		Next
+
+		Return 0 'apparently its a tie
 	End Function
 End Class
