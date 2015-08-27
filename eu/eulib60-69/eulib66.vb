a9163d621621f547784a11152668380ba9e6b559
captainkout@gmail.com <captainkout@gmail.com>
2015-08-27 20:25:15 -0300

    ohh yah!

--- eu/eulib60-69/eulib66.vb
+++ eu/eulib60-69/eulib66.vb
@@ -2,33 +2,30 @@
 
 Public Class eulib66
 	Public Sub Main()
-		'Dim lst As New List(Of Long)
-		'Dim l As Long
-		'Dim max As Long
-		'max = 10000000
+		Dim test As Long = 0
+		Dim grr As Double = 2
+		For d As Double = 2 To 1000
 
-		'For a As Long = 0 To max
-		'	l = a * a
-		'	lst.Add(l)
-		'Next
-		Dim grr As Double = 0
-		For d As Double = 2 To 50
-
-			If Math.Floor(Math.Sqrt(d)) = Math.Sqrt(d) Then Continue For
-			Dim x As Double = 2
-
+			If Math.Floor(Math.Sqrt(d)) = Math.Sqrt(d) Then
+				Console.WriteLine("{0}" & vbTab & "{1}" & vbTab & "{2}" & vbTab & "{3}", "...", d, "...", "...")
+				Continue For
+			End If
+			Dim x As Double = Max(2, Math.Floor(d / grr))
 			While True
 				grr = Math.Sqrt((x * x - 1) / d)
 				If Math.Floor(grr) = grr Then
-					Console.WriteLine("{0}" & vbTab & "{1}" & vbTab & "{2}", x, d, "boobs")
+					If test < x Then
+						test = x
+						Console.WriteLine("{0}" & vbTab & "{1}" & vbTab & "{2}" & vbTab & "{3}", test, d, grr, x / grr)
+					End If
+
 					Exit While
 				End If
-
 				x = x + 1
 			End While
 		Next
 
-		Console.WriteLine(Numerics.BigInteger.GreatestCommonDivisor(9801, 169))
+
 
 	End Sub
 End Class
