14e5aa0a479c3b23083e34148a553be10bf03703
captainkout <captainkout@gmail.com>
2015-08-28 16:26:13 -0400

    do it weekend

--- eu/eulib60-69/eulib66.vb
+++ eu/eulib60-69/eulib66.vb
@@ -2,29 +2,53 @@
 
 Public Class eulib66
 	Public Sub Main()
-		Dim test As Long = 0
-		Dim grr As Double = 2
-		For d As Double = 2 To 1000
+		Dim a As New Numerics.BigInteger
+		Dim b As New Numerics.BigInteger
+		Dim N = 67, k = 0
+		Dim m As New Numerics.BigInteger
 
-			If Math.Floor(Math.Sqrt(d)) = Math.Sqrt(d) Then
-				Console.WriteLine("{0}" & vbTab & "{1}" & vbTab & "{2}" & vbTab & "{3}", "...", d, "...", "...")
-				Continue For
-			End If
-			Dim x As Double = Max(2, Math.Floor(d / grr))
-			While True
-				grr = Math.Sqrt((x * x - 1) / d)
-				If Math.Floor(grr) = grr Then
-					If test < x Then
-						test = x
-						Console.WriteLine("{0}" & vbTab & "{1}" & vbTab & "{2}" & vbTab & "{3}", test, d, grr, x / grr)
-					End If
 
-					Exit While
-				End If
-				x = x + 1
+		'find trivial
+		a = Math.Floor(Math.Sqrt(N))
+		b = 1
+		k = a * a - N * b * b
+
+		'Console.WriteLine(B.Sign())
+
+		While Not k = 1
+			'find min k value
+			Dim kindex1 = 0, minimal = 9999999
+			m = Math.Abs(k)
+			Dim tmpBig As New Numerics.BigInteger
+			'go down
+			While Not (a + (a - kindex1)) Mod m = 0
+				kindex1 = kindex1 + 1
 			End While
-		Next
+			tmpBig = m * m - N
+			If tmpBig.Sign() = 1 AndAlso tmpBig < minimal Then
+				minimal = N + tmpBig.Sign() * (tmpBig)
+				m = (a - kindex1)
+			ElseIf tmpBig > -1 * minimal Then
+				minimal = N - tmpBig.Sign() * (tmpBig)
+				m = (a - kindex1)
+			End If
+			'go up
+			kindex1 = 0
+			While Not (a + (a + kindex1)) Mod m = 0
+				kindex1 = kindex1 + 1
+			End While
+			If tmpBig.Sign() = 1 AndAlso tmpBig < minimal Then
+				minimal = N + tmpBig.Sign() * (tmpBig)
+				m = (a - kindex1)
+			ElseIf tmpBig > -1 * minimal Then
+				minimal = N - tmpBig.Sign() * (tmpBig)
+				m = (a - kindex1)
+			End If
 
+			a = (a * m + N * b) / Math.Abs(k)
+			b = (a + b * m) / Math.Abs(k)
+			k = (m * m - N) / k
+		End While
 
 
 	End Sub
