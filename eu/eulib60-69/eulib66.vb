1d6a8cc713a2d1e3cfa5fd59819cd72c924183fe
captainkout@gmail.com <captainkout@gmail.com>
2015-08-28 21:56:10 -0300

    finally finished 66

--- eu/eulib60-69/eulib66.vb
+++ eu/eulib60-69/eulib66.vb
@@ -2,54 +2,74 @@
 
 Public Class eulib66
 	Public Sub Main()
-		Dim a As New Numerics.BigInteger
-		Dim b As New Numerics.BigInteger
-		Dim N = 67, k = 0
-		Dim m As New Numerics.BigInteger
-
-
-		'find trivial
-		a = Math.Floor(Math.Sqrt(N))
-		b = 1
-		k = a * a - N * b * b
-
-		'Console.WriteLine(B.Sign())
-
-		While Not k = 1
-			'find min k value
-			Dim kindex1 = 0, minimal = 9999999
-			m = Math.Abs(k)
-			Dim tmpBig As New Numerics.BigInteger
-			'go down
-			While Not (a + (a - kindex1)) Mod m = 0
-				kindex1 = kindex1 + 1
-			End While
-			tmpBig = m * m - N
-			If tmpBig.Sign() = 1 AndAlso tmpBig < minimal Then
-				minimal = N + tmpBig.Sign() * (tmpBig)
-				m = (a - kindex1)
-			ElseIf tmpBig > -1 * minimal Then
-				minimal = N - tmpBig.Sign() * (tmpBig)
-				m = (a - kindex1)
+		Dim max As New List(Of Numerics.BigInteger)
+		For N = 2 To 1000
+			If Math.Floor(Math.Sqrt(N)) = Math.Sqrt(N) Then
+				Continue For
 			End If
-			'go up
-			kindex1 = 0
-			While Not (a + (a + kindex1)) Mod m = 0
-				kindex1 = kindex1 + 1
+			Dim a As New Numerics.BigInteger
+			Dim b As New Numerics.BigInteger
+			Dim k = 0
+			Dim m As New Numerics.BigInteger
+
+			Dim a2 As New Numerics.BigInteger
+			Dim b2 As New Numerics.BigInteger
+			Dim m2 As New Numerics.BigInteger
+
+			'find trivial
+			a = Math.Floor(Math.Sqrt(N))
+			b = 1
+			k = a * a - N * b * b
+			m = 1
+
+			While Not k = 1
+
+				'find min k value
+				Dim minimal = 9999999
+				Dim tmpBig As New Numerics.BigInteger
+
+				'go down from rootN
+				Dim kindex1 = 0
+				While Not (a + b * (Math.Floor(Math.Sqrt(N)) - kindex1)) Mod Math.Abs(k) = 0
+					kindex1 = kindex1 + 1
+				End While
+
+				m = (Math.Floor(Math.Sqrt(N)) - kindex1)
+				tmpBig = m * m - N
+				If tmpBig.Sign() * tmpBig < minimal Then
+					minimal = tmpBig.Sign() * (tmpBig)
+				End If
+
+				'go up from rootN
+				kindex1 = 1
+				While Not (a + b * (Math.Floor(Math.Sqrt(N)) + kindex1)) Mod Math.Abs(k) = 0
+					kindex1 = kindex1 + 1
+				End While
+
+				m2 = Math.Floor(Math.Sqrt(N)) + kindex1
+				tmpBig = m2 * m2 - N
+				If tmpBig.Sign() * tmpBig < minimal Then
+					m = m2
+				End If
+				a2 = a
+				b2 = b
+				a = (a * m + N * b) / Math.Abs(k)
+				b = (a2 + b2 * m) / Math.Abs(k)
+				k = (m * m - N) / k
 			End While
-			If tmpBig.Sign() = 1 AndAlso tmpBig < minimal Then
-				minimal = N + tmpBig.Sign() * (tmpBig)
-				m = (a - kindex1)
-			ElseIf tmpBig > -1 * minimal Then
-				minimal = N - tmpBig.Sign() * (tmpBig)
-				m = (a - kindex1)
+
+			If max.Count() = 0 Then
+				max.Add(N)
+				max.Add(a)
+			ElseIf a > max(1) Then
+				max(0) = N
+				max(1) = a
 			End If
 
-			a = (a * m + N * b) / Math.Abs(k)
-			b = (a + b * m) / Math.Abs(k)
-			k = (m * m - N) / k
-		End While
 
+		Next
+
+		Console.WriteLine("{0}  {1}", max(0), max(1))
 
 
 	End Sub
