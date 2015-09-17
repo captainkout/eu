7af8f6023508daf713f3116259ee97c200cb7e72
captainkout <captainkout@gmail.com>
2015-09-17 16:52:06 -0400

    whatever

--- eu/eulib80-89/eulib86.vb
+++ eu/eulib80-89/eulib86.vb
@@ -1,28 +1,26 @@
 ﻿Public Class eulib86
     Public Sub Main()
-        Dim lim As Integer = 0
-        Dim q As Integer = 0
+        Dim x As Integer = 0
+        Dim c As Integer
         Dim count As Integer
 
-        While count < 2000
-            lim = lim + 1
-            'count = 0
-            Dim x = lim
+        While count < 1000000
+            x = x + 1
             For y = x To 1 Step -1
                 For z = y To 1 Step -1
-                    'rotation case
-                    'If x <> y AndAlso y = z Then Continue For
-                    q = Math.Min(Math.Min((x + y) ^ 2 + z ^ 2, (x + z) ^ 2 + y ^ 2), (y + z) ^ 2 + x ^ 2)
-                    If Math.Sqrt(q) = Math.Floor(Math.Sqrt(q)) Then
+                    'a = (x + y) ^ 2 + z ^ 2
+                    'b = (x + z) ^ 2 + y ^ 2
+                    c = (y + z) ^ 2 + x ^ 2
+                    'q = Math.Min(a, Math.Min(b, c))
+                    If Math.Sqrt(c) = Math.Floor(Math.Sqrt(c)) Then
                         count = count + 1
-                        'Console.WriteLine(String.Join(vbTab, {"{0}", "{1}", "{2}", "{3}"}), x, y, z, Math.Sqrt(q))
+                        'Console.WriteLine(String.Join(vbTab, {"{0}", "{1}", "{2}", "{3}", "{4}"}), x, y, z, Math.Sqrt(q), count)
                     End If
                 Next
             Next
-
-
         End While
-
+        'two min....
+        Console.WriteLine("{0}" & vbTab & "{1}", x, count)
 
     End Sub
 End Class
