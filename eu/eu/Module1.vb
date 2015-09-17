b3573e035d181901c05615e09cef8d10aaa36409
captainkout <captainkout@gmail.com>
2015-09-17 15:45:14 -0400

    almost 86

--- eu/eu/Module1.vb
+++ eu/eu/Module1.vb
@@ -4,7 +4,7 @@ Module Module1
 	Sub Main()
 
 
-		Dim y As New eulib80_89.eulib85
+        Dim y As New eulib80_89.eulib86
 
 		GC.Collect()
 		GC.WaitForFullGCComplete()
---
+++ eu/eulib80-89/eulib86.vb
@@ -0,0 +1,28 @@
+﻿Public Class eulib86
+    Public Sub Main()
+        Dim lim As Integer = 0
+        Dim q As Integer = 0
+        Dim count As Integer
+
+        While count < 2000
+            lim = lim + 1
+            'count = 0
+            Dim x = lim
+            For y = x To 1 Step -1
+                For z = y To 1 Step -1
+                    'rotation case
+                    'If x <> y AndAlso y = z Then Continue For
+                    q = Math.Min(Math.Min((x + y) ^ 2 + z ^ 2, (x + z) ^ 2 + y ^ 2), (y + z) ^ 2 + x ^ 2)
+                    If Math.Sqrt(q) = Math.Floor(Math.Sqrt(q)) Then
+                        count = count + 1
+                        'Console.WriteLine(String.Join(vbTab, {"{0}", "{1}", "{2}", "{3}"}), x, y, z, Math.Sqrt(q))
+                    End If
+                Next
+            Next
+
+
+        End While
+
+
+    End Sub
+End Class
